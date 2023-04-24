26f4d8e8295d115bc8f03ea07505e3e7929479c6
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-24 21:59:18 +0500

    properties for variables

--- Date Class/Program.cs
+++ Date Class/Program.cs
@@ -11,4 +11,18 @@
         get { return month; }
         set { month = value; }
     }
+
+    // Day property for setting and getting value for day variable
+    int Day
+    {
+        get { return day; }
+        set { day = value; }
+    }
+
+    // Year property for setting and getting value for year variable
+    int Year
+    {
+        get { return year; }
+        set { year = value; }
+    }
 }
