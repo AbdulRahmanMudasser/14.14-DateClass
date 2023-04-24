c13f124830716d3d6feff95405d0c7b64b652049
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-24 21:57:24 +0500

    Add project files.

---
+++ Date Class/Program.cs
@@ -0,0 +1,14 @@
+﻿class Date
+{
+    // declaring variables
+    int month;
+    int day;
+    int year;
+
+    // Month property for setting and getting value for month variable
+    int Month
+    {
+        get { return month; }
+        set { month = value; }
+    }
+}
